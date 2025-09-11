b02b1fedc4e0463f03f3bcb26e68772c10064762
hycx233 <hycx2333@outlook.com>
2025-09-11 12:43:03 +0800

    feat: implement math eval function

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -5,6 +5,8 @@
  */
 #include <sys/types.h>
 #include <regex.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 enum {
 	NOTYPE = 256,
@@ -127,7 +129,107 @@ static bool make_token(char *e) {
 		}
 	}
 
-	return true; 
+	return true;
+}
+
+static bool check_parentheses(int l, int r, bool *success) {
+	if (tokens[l].type != LPAREN || tokens[r].type != RPAREN) return false;
+
+	int balance = 0;
+	int i;
+	for (i = l; i <= r; i++) {
+		if (tokens[i].type == LPAREN) balance++;
+		else if (tokens[i].type == RPAREN) balance--;
+
+		if (balance == 0 && i < r) return false;
+		if (balance < 0) {
+			*success = false;
+			return false;
+		}
+	}
+
+	if (balance != 0) {
+		*success = false;
+		return false;
+	}
+
+	return true;
+}
+
+static int find_dominant_op(int l, int r, bool *success) {
+	int min_pri = 100, op = -1, balance = 0;
+	int i;
+	for (i = l; i <= r; i++) {
+		if (tokens[i].type == LPAREN) balance++;
+		else if (tokens[i].type == RPAREN) balance--;
+		if (balance != 0) continue; // skip tokens inside parentheses
+
+		int pri = -1;
+		switch (tokens[i].type) {
+			case EQ: pri = 1; break;
+			case PLUS:
+			case MINUS: pri = 2; break;
+			case MULTIPLY:
+			case DIVIDE: pri = 3; break;
+			default: break;
+		}
+		// find the rightmost operator with the lowest precedence
+		if (pri >= 0 && pri < min_pri) {
+			min_pri = pri;
+			op = i;
+		}
+	}
+	if (op == -1) {
+		*success = false;
+	}
+	return op;
+}
+
+static uint32_t eval(int l, int r, bool *success) {
+	if (!*success) {
+		return 0;
+	}
+
+	if (l > r) {
+		*success = false;
+		return 0;
+	}
+
+	if (l == r) {
+		if (tokens[l].type == NUMBER) {
+			return (uint32_t)strtoul(tokens[l].str, NULL, 10);
+		} else {
+			*success = false;
+			return 0;
+		}
+	}
+
+	if (check_parentheses(l, r, success)) {
+		return eval(l + 1, r - 1, success);
+	}
+
+	int op = find_dominant_op(l, r, success);
+	if (!*success) return 0;
+	uint32_t val1 = eval(l, op - 1, success);
+	if (!*success) return 0;
+	uint32_t val2 = eval(op + 1, r, success);
+	if (!*success) return 0;
+
+	switch (tokens[op].type) {
+		case PLUS: return val1 + val2;
+		case MINUS: return val1 - val2;
+		case MULTIPLY: return val1 * val2;
+		case DIVIDE:
+			if (val2 == 0) {
+				*success = false;
+				return 0;
+			}
+			return val1 / val2;
+		case EQ: return val1 == val2;
+		default:
+			*success = false;
+			return 0;
+	}
 }
 
 uint32_t expr(char *e, bool *success) {
@@ -135,9 +237,6 @@ uint32_t expr(char *e, bool *success) {
 		*success = false;
 		return 0;
 	}
-
-	/* TODO: Insert codes to evaluate the expression. */
-	panic("please implement me");
-	return 0;
+	*success = true;
+	return eval(0, nr_token - 1, success);
 }
-
