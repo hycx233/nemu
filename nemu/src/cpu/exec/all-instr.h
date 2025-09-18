bf52fe81b67feee2a0377ad72fe2322542ecf28d
hycx233 <hycx2333@outlook.com>
2025-09-19 00:00:03 +0800

    feat: implement ret instruction

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -22,6 +22,7 @@
 #include "control/jmp.h"
 #include "control/call.h"
 #include "control/jcc.h"
+#include "control/ret.h"
 
 #include "logic/and.h"
 #include "logic/or.h"
---
+++ nemu/src/cpu/exec/control/ret-template.h
@@ -0,0 +1,22 @@
+#include "cpu/exec/template-start.h"
+
+#define instr ret
+
+make_helper(concat(ret_n_, SUFFIX)) {
+	DATA_TYPE_S ret_addr = swaddr_read(cpu.esp, DATA_BYTE);
+	cpu.esp += DATA_BYTE;
+	cpu.eip = ret_addr;
+	print_asm("ret");
+	return 0;
+}
+
+make_helper(concat(ret_i_, SUFFIX)) {
+	uint16_t imm = instr_fetch(eip + 1, 2);
+	DATA_TYPE_S ret_addr = swaddr_read(cpu.esp, DATA_BYTE);
+	cpu.esp += DATA_BYTE + imm;
+	cpu.eip = ret_addr;
+	print_asm("ret $0x%x", imm);
+	return 2;
+}
+
+#include "cpu/exec/template-end.h"
