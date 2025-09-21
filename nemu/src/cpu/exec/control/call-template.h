1dfc7b81dfb362bece8b85f617215de65dbf9fbb
hycx233 <hycx2333@outlook.com>
2025-09-21 15:55:05 +0800

    refactor: update call and push instruction handling for improved functionality

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -2,20 +2,23 @@
 
 #define instr call
 
-static void do_execute() {
-	// calc return address
-	swaddr_t return_addr = cpu.eip + 1 + DATA_BYTE;
-	
-	// push return address onto stack
-	cpu.esp -= 4;
-	swaddr_write(cpu.esp, 4, return_addr);
-	
-	// jump to target address
-	cpu.eip += op_src->val;
-	
-	print_asm("call %x", cpu.eip + 1 + DATA_BYTE);
+make_helper(concat(call_i_, SUFFIX)) {
+	int len = concat(decode_i_, SUFFIX)(cpu.eip + 1);
+    reg_l(R_ESP) -= DATA_BYTE;
+    swaddr_write(reg_l(R_ESP), 4, cpu.eip + len + 1);
+    DATA_TYPE_S imm = op_src -> val;
+    print_asm("call\t%x",cpu.eip + 1 + len + imm);
+    cpu.eip += imm;
+    return len + 1;
+} 
+
+make_helper(concat(call_rm_, SUFFIX)){
+    int len = concat(decode_rm_, SUFFIX)(cpu.eip + 1);
+	reg_l(R_ESP) -= DATA_BYTE;
+	swaddr_write(reg_l(R_ESP) , 4, cpu.eip + len + 1);
+	DATA_TYPE_S imm = op_src -> val;
+	print_asm("call %x",imm);
+	cpu.eip = imm - len - 1;
+	return len + 1;
 }
-
-make_instr_helper(si)
-
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/control/call.h
+++ nemu/src/cpu/exec/control/call.h
@@ -1,6 +1,9 @@
 #ifndef __CALL_H__
 #define __CALL_H__
 
-make_helper(call_si_l);
+make_helper(call_i_v);
+
+make_helper(call_rm_v);
+
 
 #endif
